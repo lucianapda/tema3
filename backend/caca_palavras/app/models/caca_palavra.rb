e8d3e4dc58a2383e2dd7941714b8717c3a011071
Rafael Sabel <rafael.sabel@hotmail.com>
2017-09-18 01:10:19 -0300

    adicionados validates e método para criar o caça palavras

--- backend/caca_palavras/app/models/caca_palavra.rb
+++ backend/caca_palavras/app/models/caca_palavra.rb
@@ -7,5 +7,20 @@ class CacaPalavra < ActiveRecord::Base
 
   has_many :notas
  
-  validates :nome, presence: true 
+  validates :nome, presence: true
+
+  #before_create :create_caca_palavras
+  before_save :valida_palavras_iguais
+
+  private
+
+  def valida_palavras_iguais
+  	return if palavras.map(&:palavra).uniq.size == palavras.map(&:palavra).size
+	raise 'Não são permitidas palavras iguais.'  	
+  end
+
+  def create_caca_palavras
+  	#https://github.com/jamis/wordsearch/blob/master/bin/wordsearch
+  	WordSearch::Puzzle.new(palavras.map(&:palavra))
+  end
 end
