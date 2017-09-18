c90c0d0aea27470d3a10f2ad8f6ff66d670b3252
Rafael Sabel <rafael.sabel@hotmail.com>
2017-09-18 01:10:41 -0300

    adicionado método para validar esxlusão da palavra

--- backend/caca_palavras/app/models/palavra.rb
+++ backend/caca_palavras/app/models/palavra.rb
@@ -1,5 +1,13 @@
 class Palavra < ActiveRecord::Base
   belongs_to :caca_palavra
 
-  validates :palavra, presence: true 
+  validates :palavra, presence: true
+
+  before_destroy :valida_exclusao
+
+  private
+
+  def valida_exclusao
+  	raise 'Não é possível excluir uma palavra do jogo.'
+  end
 end
