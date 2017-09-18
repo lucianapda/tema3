acd2e4ac2229dbfa3aab12823de0a58142a8efbe
Rafael Sabel <rafael.sabel@hotmail.com>
2017-09-18 01:08:53 -0300

    ajustado controller de usuários

--- backend/caca_palavras/app/controllers/usuarios_controller.rb
+++ backend/caca_palavras/app/controllers/usuarios_controller.rb
@@ -4,7 +4,10 @@ class UsuariosController < ApplicationController
 
   def index
     usuarios = Usuario.select(:id, :nome, :email, :tipo)
-    usuarios = usuarios.where(params[:filters])
+
+    usuarios = usuarios.where(nome: params[:filters][:nome]) if params[:filters][:nome].present?
+    usuarios = usuarios.where(email: params[:filters][:email]) if params[:filters][:email].present?
+    usuarios = usuarios.where(tipo: params[:filters][:tipo]) if params[:filters][:tipo].present?
 
     respond_with usuarios
   end
